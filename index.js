e4f43a6a6eb3a0c29d7c662a43630a4089a12dfa
Sajjad Hashemian <wolaws@gmail.com>
2019-07-30 21:01:22 +0430

    remove old codes

--- index.js
+++ index.js
@@ -1,5 +1,1 @@
-import { AppRegistry } from 'react-native';
-import { App } from './src/App';
-import { name as appName } from './app.json';
-
-AppRegistry.registerComponent(appName, () => App);
+import './src/App';
